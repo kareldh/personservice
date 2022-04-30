3b3e08740a401df4752f1c04ed075695655f297b
Karel D'haene <karel.dhaene@gmail.com>
2022-04-30 14:53:28 +0200

    Initial blank sprint boot project

---
+++ src/main/java/com/example/personservice/PersonserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.personservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/personservice/PersonserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
