ee92fe0689a38e4eab547464ef92ce2150435674
Karel D'haene <karel.dhaene@gmail.com>
2022-05-01 16:46:04 +0200

    Remove kafka, add correct UUID key instead of string

--- src/main/java/com/example/personservice/person/repository/PersonRepository.java
+++ src/main/java/com/example/personservice/person/repository/PersonRepository.java
@@ -4,6 +4,8 @@ import com.example.personservice.person.model.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface PersonRepository extends CrudRepository<Person, String>, CustomPersonRepository {
+public interface PersonRepository extends CrudRepository<Person, UUID>, CustomPersonRepository {
 }
