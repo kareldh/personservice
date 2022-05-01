28998656adf0532c691ff975ac67c531afaccd78
Karel D'haene <karel.dhaene@gmail.com>
2022-05-01 16:47:18 +0200

    Update test schema

--- src/test/resources/database/schema_hr.sql
+++ src/test/resources/database/schema_hr.sql
@@ -2,6 +2,8 @@ CREATE SCHEMA if not exists hr;
 
 CREATE TABLE if not exists hr.person (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    firstName varchar(128),
+    lastName varchar(128),
     contractStartDate date,
     contractEndDate date
 );
