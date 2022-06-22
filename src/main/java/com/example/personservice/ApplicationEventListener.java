93d4a254b843a716e903f7f429c6dbb2c74f009d
Karel D'haene <karel.dhaene@gmail.com>
2022-06-22 19:35:12 +0200

    disable/comment everything

--- src/main/java/com/example/personservice/ApplicationEventListener.java
+++ src/main/java/com/example/personservice/ApplicationEventListener.java
@@ -4,9 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class ApplicationEventListener {
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationEventListener.class);
 
